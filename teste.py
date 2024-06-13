bbc0ae464ac0128842992f0bf2f0d51a2f59aafe
matheusghb <ofduardela@gmail.com>
2024-06-13 14:37:02 -0300

    add py atv

---
+++ teste.py
@@ -0,0 +1,23 @@
+adicao = 10 + 10
+print('Adição', adicao)
+
+subtracao = 10 - 5 
+print('Subtração', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 3 # float(value)
+print('Divisão', divisao)
+
+exponencial = 2 **  10
+print('Exponencial', exponencial)
+
+modulo = 55 % 2 #resto da divisão
+print('Módulo', modulo)
+
+print(10 % 8 == 0)
+print(16 % 8 == 0)
+print(10 % 2 == 0)
+print(15 % 2 == 0)
+print(16 % 2 == 0)
